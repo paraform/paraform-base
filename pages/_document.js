48b2b57398d9813365a1cf4c6ad4fca0cb347040
Paraform <dean@paraform.co.uk>
2020-04-25 16:14:09 +0100

    Add PWA support

---
+++ pages/_document.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+          <link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#000000"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="msapplication-config" content="/browserconfig.xml" />
+          <meta name="theme-color" content="#000000" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import Head from "next/head";
+
 function Home() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <>
+      <Head>
+        <title>App Template | Paraform</title>
+        <meta
+          name="description"
+          content="Template repo for a static next js web app with PWA support, sanity cms and theme-ui."
+        />
+      </Head>
+      <div>Welcome to Paraform!</div>
+    </>
+  );
 }
 
 export default Home;
