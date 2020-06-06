0de3a1ffe256d3d0ee0c68d9ab5aa82f81108fc0
Paraform <code@paraform.studio>
2020-06-06 17:31:22 +0100

    Add vercel deploy button to cms

---
+++ cms/src/dashboardConfig.js
@@ -0,0 +1,17 @@
+export default {
+  widgets: [
+    // { name: "structure-menu" },
+    { name: "deploy-vercel" },
+    { name: "project-users", layout: { height: "auto" } },
+    // {
+    //   name: "document-list",
+    //   options: {
+    //     title: "Recently edited",
+    //     order: "_updatedAt desc",
+    //     limit: 10,
+    //     types: ["page"],
+    //   },
+    //   layout: { width: "medium" },
+    // },
+  ],
+};
