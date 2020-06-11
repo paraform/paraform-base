0361bd39ae4f25d228aa1de5393bb7388455c82b
Paraform <code@paraform.studio>
2020-06-11 19:59:32 +0100

    change name to property on og meta

--- src/library/components/meta.js
+++ src/library/components/meta.js
@@ -3,12 +3,12 @@ import Head from "next/head";
 export default ({ title, name, description, image, url }) => (
   <Head>
     <title>{title}</title>
+    {description && <meta name="description" content={description} />}
     <meta property="og:type" content="website" />
     {title && <meta name="og:title" content={title} />}
     <meta property="og:site_name" content={name} />
-    {url && <meta name="og:url" content={url} />}
-    {description && <meta name="description" content={description} />}
-    {description && <meta name="og:description" content={description} />}
-    {image && <meta name="og:image" content={image} />}
+    {url && <meta property="og:url" content={url} />}
+    {description && <meta property="og:description" content={description} />}
+    {image && <meta property="og:image" content={image} />}
   </Head>
 );
